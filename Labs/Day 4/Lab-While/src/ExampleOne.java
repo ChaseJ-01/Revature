b7f740280dc727383b3c1f2d7243ec8b7f411b50
ChaseJ-01 <chase.jones@revature.net>
2021-07-14 14:45:04 -0400

    Added day 4 labs

---
+++ Labs/Day 4/Lab-While/src/ExampleOne.java
@@ -0,0 +1,13 @@
+import java.awt.datatransfer.SystemFlavorMap;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		boolean on = true;
+		
+		while (on) {
+			System.out.println("Inside the while loop");
+			//on = false;
+		}
+	}
+}
